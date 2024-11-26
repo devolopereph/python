0d22de5051e6cde9bc2563dc2fe516a35f13bd11
devolopereph <efelstoy@gmail.com>
2024-11-26 23:02:29 +0300

    Add files via upload

---
+++ en-buyuk-sayi.py
@@ -0,0 +1,13 @@
+sayi1 = int(input("1.sayıyı giriniz: "))
+sayi2 = int(input("2.sayıyı giriniz: "))
+sayi3 = int(input("3.sayıyı giriniz: "))
+
+en_buyuk = sayi1
+
+if sayi2 > en_buyuk:
+    en_buyuk = sayi2
+
+if sayi3 > en_buyuk:
+    en_buyuk = sayi3
+
+print(f"En büyük sayı: {en_buyuk}")
