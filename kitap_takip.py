4a11d191f45ef9a651bebaef5c8d0dcca085be74
devolopereph <efelstoy@gmail.com>
2024-12-08 03:56:56 +0300

    Add files via upload

---
+++ kitap_takip.py
@@ -0,0 +1,23 @@
+# Kullanıcıdan okumak istediği kitabın adını ve toplam sayfasını öğreneceğiz ve kullanıcıya her zaman ne kadar okuduğunu soracağız, eğer okunan sayfa sayısı toplam sayfa sayısını geçerse başka bir kitabın verilerini alacağız, bu şekilde döngü sonsuza kadar devam edecek.
+
+import os
+
+while True:
+    toplam_sayfa=0
+    okunan_sayfa=0
+    kitap_ad = ""
+    print("Kitap takip sistemi başlatılıyor...")
+    kitap_ad = str(input("Okumaya başlayacağınız kitabın adını giriniz: "))
+    toplam_sayfa = int(input(f"{kitap_ad} adlı kitabın toplam sayfa sayısını giriniz: "))
+    while True:
+        if okunan_sayfa < toplam_sayfa:
+            os.system('cls')
+            print(f"Okumakta olduğunuz kitap: {kitap_ad}\nOkunan sayfa sayısı: {okunan_sayfa}/{toplam_sayfa}")
+            okunan_sayfa += int(input(f"Okumuş olduğunuz sayfa sayısını giriniz: "))
+        elif okunan_sayfa >= toplam_sayfa:
+            os.system('cls')
+            print(f"{kitap_ad} adlı kitabı bitirdiniz, tebrikler.")
+            print("Döngü yeniden başlatılıyor...")
+            break
+        else:
+            print("Hatalı giriş yaptınız. Lütfen tekrar deneyin.")
