f6610ceab79e30e0c737afa01dcec37e65933f31
devolopereph <efelstoy@gmail.com>
2025-01-12 15:17:35 +0300

    Add files via upload

---
+++ tam_bolenleri_bulma.py
@@ -0,0 +1,8 @@
+#Girilen sayının tam bölenlerini bulan fonksiyonu yazalım.
+def tambolenleribulma(sayi):
+    tambolenler=[]
+    for i in range(2,sayi):
+        if sayi %i == 0:
+            tambolenler.append(i)
+    return tambolenler
+print(tambolenleribulma(int(input('sayiyi giriniz: '))))
