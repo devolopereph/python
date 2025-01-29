b770cf8460d1d75451ed1e93d42e4732e53706e2
devolopereph <efelstoy@gmail.com>
2025-01-29 21:33:21 +0300

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -1,38 +1,36 @@
 # Basit bir Quiz uygulaması yapacağız.
 import os
-class Question:
-    def __init__(self, text, choices, answer):
-        self.text = text
-        self.choices = choices
-        self.answer = answer
+class Soru:
+    def __init__(self, soru, cevaplar, cevap):
+        self.soru = soru
+        self.cevaplar = cevaplar
+        self.cevap = cevap
 
-    def readQuestion(self, index):
+    def soruOku(self, index):
         self.index=index
-        print(f'{self.index}.Soru: {self.text}')
-        for i in self.choices:
+        print(f'{self.index}.Soru: {self.soru}')
+        for i in self.cevaplar:
             print(f'- {i}')
 
-    def checkAnswer(self, answer):
-        return self.answer == answer
+    def cevapKontrol(self, cevap):
+        return self.cevap == cevap
     
-class Quiz(Question):
-    def __init__(self, text, choices, answer):
-        super().__init__(text, choices, answer)
-        self.score = 0
-        self.questionIndex = 0
+class Quiz(Soru):
+    def __init__(self, soru, cevaplar, cevap):
+        super().__init__(soru, cevaplar, cevap)
 
-q1 = Question("Python'da değişken tanımlamak için hangi sembol kullanılır?", ['=', '==', ':=', '==='], '=')
-q2 = Question("Hangisi Python’da bir döngü türüdür?", ['if-else', 'while', 'elif', 'switch'], 'while')
-q3 = Question("Python'da ekrana çıktı vermek için hangi fonksiyon kullanılır?", ['echo()', 'display()', 'print()', 'write()'], 'print()')
-q4 = Question("Python'da koşullu ifadeler hangi anahtar kelimeyle başlar?", ['if', 'when', 'case', 'switch'], 'if')
-liste = [q1,q2,q3,q4]
+q1 = Soru("Python'da değişken tanımlamak için hangi sembol kullanılır?", ['=', '==', ':=', '==='], '=')
+q2 = Soru("Hangisi Python’da bir döngü türüdür?", ['if-else', 'while', 'elif', 'switch'], 'while')
+q3 = Soru("Python'da ekrana çıktı vermek için hangi fonksiyon kullanılır?", ['echo()', 'display()', 'print()', 'write()'], 'print()')
+q4 = Soru("Python'da koşullu ifadeler hangi anahtar kelimeyle başlar?", ['if', 'when', 'case', 'switch'], 'if')
+soru_listesi = [q1,q2,q3,q4]
 tam_puan=100
-cikarilacak_puan=tam_puan//(len(liste))
-for i in range(len(liste)):
+cikarilacak_puan=tam_puan//(len(soru_listesi))
+for i in range(len(soru_listesi)):
     os.system('cls||clear')
-    liste[i].readQuestion(i+1)
-    print(f'Kalan soru: {i+1}/{len(liste)}')
-    if liste[i].checkAnswer(input('Cevabınız: ')) == False:
+    soru_listesi[i].soruOku(i+1)
+    print(f'Kalan soru: {i+1}/{len(soru_listesi)}')
+    if soru_listesi[i].cevapKontrol(input('Cevabınız: ')) == False:
         tam_puan -= cikarilacak_puan
 os.system('cls||clear')
 print(f'Quiz sona erdi.\nPuanınız: {tam_puan}')
