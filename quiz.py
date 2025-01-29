b41558a4344c6a9ee795af6759209f51f4332f2e
devolopereph <efelstoy@gmail.com>
2025-01-29 21:46:27 +0300

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -20,9 +20,11 @@ q1 = Soru("Python'da değişken tanımlamak için hangi sembol kullanılır?", [
 q2 = Soru("Hangisi Python’da bir döngü türüdür?", ['if-else', 'while', 'elif', 'switch'], 'while')
 q3 = Soru("Python'da ekrana çıktı vermek için hangi fonksiyon kullanılır?", ['echo()', 'display()', 'print()', 'write()'], 'print()')
 q4 = Soru("Python'da koşullu ifadeler hangi anahtar kelimeyle başlar?", ['if', 'when', 'case', 'switch'], 'if')
+
 soru_listesi = [q1,q2,q3,q4]
 tam_puan = 100
 cikarilacak_puan=tam_puan//(len(soru_listesi))
+
 for i in range(len(soru_listesi)):
     os.system('cls||clear')
     soru_listesi[i].soruOku(i+1)
