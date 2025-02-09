700a61bbbbf851897039e7407a4d202dda8478b9
devolopereph <efelstoy@gmail.com>
2025-02-09 08:26:51 +0300

    Add files via upload

--- basit_login.py
+++ basit_login.py
@@ -1,15 +1,14 @@
-#Kullanıcının sisteme giriş yapıp yapamamasını sağlayan kodu yazıyoruz.
-#Doğru kullanıcı bilgilerini tuttuğumuz kısım.
+import os
 username = 'admin'
 password = 'admin'
 
-#Kullanıcı bilgilerini sorduğumuz kısım.
-usernameinput = input("Kullanıcı adınızı giriniz: ")
-passwordinput = input("Kullanıcı şifrenizi giriniz: ")
-
-#Şartlar doğruysa, if, yani eğer çalışacaktır ve hoş geldiniz mesajı gösterilecektir.
-if (usernameinput == username) and (passwordinput == password):
-    print("Hoş geldiniz.")
-#Eğer, ifde belirttiğimiz şartlar doğru değil ise, else komutu çalışacaktır.
-else:
-    print("Lütfen bilgilerinizi kontrol ediniz.")
+while True:
+    usernameinput = input("Kullanıcı adınızı giriniz: ")
+    passwordinput = input("Kullanıcı şifrenizi giriniz: ")
+    if (usernameinput == username) and (passwordinput == password):
+        print("Hoş geldiniz.")
+        break
+    else:
+        os.system('cls||clear')
+        print("Kullanıcı adınız veya şifreniz yanlış. Lütfen tekrar deneyiniz.")
+        continue
